1e7ef2faeb57e41081422382369fc3183c2e7aaf
Nicola Corato <nicolacorato05@gmail.com>
2022-01-30 00:06:35 +0100

    init

---
+++ Menu.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace MenuCreator
+{
+    static class MenuUtils
+    {
+        public static void WriteColor(string msg, ConsoleColor c)
+        {
+            string[] pieces = Regex.Split(msg, @"(\[[^\]]+\])");
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                if(pieces[i].StartsWith("[") && pieces[i].EndsWith("]"))
+                {
+                    Console.ForegroundColor = c;
+                    pieces[i] = pieces[i].Substring(i, pieces[i].Length - 2);
+                }
+                Console.Write(pieces[i]);
+                Console.ResetColor();
+            }
+        }
+        public static void ClearLine(int top)
+        {
+            Console.SetCursorPosition(0, top);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, top);
+        }
+        public static int CreateMenu(string[] options, string title)
+        {
+            // Scrittura del titolo
+            Console.WriteLine(title);
+
+            int menuStartTop = Console.CursorTop;
+            
+            // Impostazione del cursore a invisibile per estetica
+            Console.CursorVisible = false;
+            
+            // Scrittura delle opzioni
+            for (int i = 0; i < options.Length; i++)
+            {
+                WriteColor($"[>] {options[i]}\n", ConsoleColor.White);
+            }
+
+            int currentOption = menuStartTop;
+            int oldOption = menuStartTop;
+
+            Console.SetCursorPosition(0, currentOption);
+            WriteColor("[>]", ConsoleColor.Blue);
+
+            ConsoleKeyInfo key;
+
+            do
+            {
+                key = Console.ReadKey(true);
+                switch (key.Key)
+                {
+                    // Navigazione delle opzioni
+                    case ConsoleKey.UpArrow:
+                        currentOption = currentOption > menuStartTop ? currentOption - menuStartTop : menuStartTop + (options.Length - 1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        currentOption = currentOption < options.Length ? menuStartTop + ((currentOption) % options.Length) : menuStartTop;
+                        break;
+                }
+                Console.SetCursorPosition(0, oldOption);
+                WriteColor("[>]", ConsoleColor.White);
+
+                Console.SetCursorPosition(0, currentOption);
+                WriteColor("[>]", ConsoleColor.Blue);
+
+                oldOption = currentOption;
+
+            } while (key.Key != ConsoleKey.Enter);
+            
+            // Cancellazione del menu dalla console
+            for (int i = options.Length - 1; i >= menuStartTop - 1; i--)
+            {
+                ClearLine(i);
+            }
+            // Restituzione dell'indice dell'opzione partendo da 0
+            return currentOption - menuStartTop;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Menu Utils
+A simple library to create simple CUI menus that work using the arrow keys  
+
+## Usage
+At the top of your program write ```cs
+using MenuCreator```  
+
+then in your file, when you want to create the menu call the CreateMenu function in the MenuUtils class.
+This function returns an index of the option which has been selected (indexes start at 0)
+ex. ```cs
+int scelta = MenuUtils.CreateMenu(new string[] { "Salame", "Pesce", "Carne", "Ho finito i tipi di panini :(" }, "Che tipo di panino ti piace?");
+switch(scelta){
+	...
+}
+```
